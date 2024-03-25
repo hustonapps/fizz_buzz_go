ef7c2ddda4b7ed26a65bcb77243d2f6c22680e66
Matt Huston <hustonapps@gmail.com>
2024-03-25 05:39:49 -0600

    simpler approach

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	x := rand.Intn(45) + 1
 	println(x)
 	if x % 3 == 0 {
-		if x % 5 == 0 {
-			println("FizzBuzz")
-		} else {
-			println("Fizz")
-		}
-	} else if x % 5 == 0 {
-		println("Buzz")
+		fmt.Print("Fizz")
 	}
+	if x % 5 == 0 {
+		fmt.Print("Buzz")
+	}
+	println()
 }
